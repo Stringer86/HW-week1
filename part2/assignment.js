b5a1a10fca82eae0e7b27b8918bce89b7b58ab48
Stringer86 <stringer.stephen@gmail.com>
2016-08-13 15:53:39 -0700

    part 1 and part 2 complete

--- part2/assignment.js
+++ part2/assignment.js
@@ -313,3 +313,10 @@ function toPairs(obj) {
 // Return a new object where each key-value pair is from an element in the
 // argument. For example, given [['a', 1], ['b', 2]], then return
 // { a: 1, b: 2 }.
+function fromPairs(arr) {
+  var obj = {};
+  for (var i = 0; i < arr.length; i++) {
+    obj[arr[i][0]] = arr[i][1];
+  }
+  return obj;
+}
